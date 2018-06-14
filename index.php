f3d77e7dc2857e62daeec1e379655545e85966ce
zsavich <zsavich59@gmail.com>
2018-06-14 12:28:10 +0300

    Добавил категории и объявления

--- index.php
+++ index.php
@@ -3,6 +3,45 @@ $is_auth = (bool) rand(0, 1);
 
 $user_name = 'Константин';
 $user_avatar = 'img/user.jpg';
+$categories = ["Доски и лыжи","Крепления","Ботинки","Одежда","Инструменты","Разное"];
+$ads = [
+        [
+            'title' => '2014 Rossiglon District Snowboard',
+            'categories' => 'Доски и лыжи',
+            'price' => 10999,
+            'image' => 'img/lot-1.jpg'
+        ],
+        [
+            'title' => 'DC Ply Mens 2016/2017 Snowboard',
+            'categories' => 'Доски и лыжи',
+            'price' => 159999,
+            'image' => 'img/lot-2.jpg'
+        ],
+        [
+            'title' => 'Креаления Union Contact Pro 2015 года размер L/XL',
+            'categories' => 'Крепления',
+            'price' => 8000,
+            'image' => 'img/lot-3.jpg'
+        ],
+        [
+            'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
+            'categories' => 'Ботинки',
+            'price' => 10999,
+            'image' => 'img/lot-4.jpg'
+        ],
+        [
+            'title' => 'Куртка для сноуборда DC Mutiny Charocal',
+            'categories' => 'Одежда',
+            'price' => 7500,
+            'image' => 'img/lot-5.jpg'
+        ],
+        [
+            'title' => 'Маска Oakley Canopy',
+            'categories' => 'Разное',
+            'price' => 5400,
+            'image' => 'img/lot-6.jpg'
+        ]
+];
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -78,17 +117,18 @@ $user_avatar = 'img/user.jpg';
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
+            <?php foreach($ads as $key => $value): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="img/lot-1.jpg" width="350" height="260" alt="Сноуборд">
+                    <img src="<?=$value['image'];?>" width="350" height="260" alt="Сноуборд">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Доски и лыжи</span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.html">2014 Rossignol District Snowboard</a></h3>
+                    <span class="lot__category"><?=$value['categories'];?></span>
+                    <h3 class="lot__title"><a class="text-link" href="lot.html"><?=$value['title'];?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">10 999<b class="rub">р</b></span>
+                            <span class="lot__cost"><?=$value['price'];?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
 
@@ -96,6 +136,7 @@ $user_avatar = 'img/user.jpg';
                     </div>
                 </div>
             </li>
+            <?php endforeach; ?>
         </ul>
     </section>
 </main>
@@ -103,24 +144,13 @@ $user_avatar = 'img/user.jpg';
 <footer class="main-footer">
     <nav class="nav">
         <ul class="nav__list container">
+            <?php
+            $index = 0;
+            while($index < count($categories)) { ?>
             <li class="nav__item">
-                <a href="all-lots.html">Доски и лыжи</a>
+                <a href="all-lots.html"><?=$categories[$index];?></a>
             </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Крепления</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Ботинки</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Одежда</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Инструменты</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Разное</a>
-            </li>
+            <?php $index++; } ?>
         </ul>
     </nav>
     <div class="main-footer__bottom container">
